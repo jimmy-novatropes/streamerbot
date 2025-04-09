20b9539bd472edb05d68190d5aa4ac3bfc9ce72b
nodnarb93 <bpkeeber@gmail.com>
2025-04-08 19:12:01 -0700

    updates to the code for reduction in redundancy

--- current code/5_update_time_text.cs
+++ current code/5_update_time_text.cs
@@ -6,7 +6,7 @@ public class CPHInline
     {
         // Retrieve the next user from the global variables
 		var time_left = CPH.GetGlobalVar<string>("time_left");
-		string next_in_line = $"\n Next View in {time_left} minutes";
+		string next_in_line = $"\n Next View in {time_left}";
 		// Store the message in a global variable
 		CPH.SetGlobalVar("next_in_line", next_in_line);
 
